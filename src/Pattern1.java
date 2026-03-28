5abf6a8b82bbf5ba68b7a02d5bb4ea4d4316801e
KumarSurajwebdeveloper <kr.surajkumar09@gmail.com>
2026-03-29 00:38:12 +0530

    Tasks added

--- src/Pattern1.java
+++ src/Pattern1.java
@@ -12,6 +12,10 @@ public class Pattern1 {
 
         List<Object> input = Arrays.asList("A", "B", "C", "1", "2", "3", "4", 1, 2, 3, "a", "b");
 
+        List <String> input2 = List.of("apple", "banana", "apple", "orange", "banana", "kiwi");
+
+        String[] input3 = {"apple", "banana", "apple", "orange", "banana", "kiwi"};
+
         // 🔥 STEP 1 — Create Frequency Map
         Map<String, Long> freqMap = list.stream()
                 .collect(Collectors.groupingBy(
@@ -64,6 +68,71 @@ public class Pattern1 {
                 .toList());
 
         System.out.println("Unique Element: " + result);
+
+
+//        Question: Input: ["apple", "banana", "apple", "orange", "banana", "kiwi"]
+//        Find: 1. Unique elements
+//        2. Duplicate elements
+//        3. First non-repeating element
+
+        //    Find: 1. Unique elements
+        List<String> UniqueFruits = Arrays.stream(input3)
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        LinkedHashMap::new,
+                        Collectors.counting()
+                ))
+                .entrySet().stream()
+                .filter(entry -> entry.getValue() == 1)
+                .map(Map.Entry::getKey)
+                .toList();
+
+        System.out.println("Unique Fruits: " + UniqueFruits);
+
+
+        String[] UniqueFruits2 = Arrays.stream(input3)
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        LinkedHashMap::new,
+                        Collectors.counting()
+                ))
+                .entrySet().stream()
+                .filter(entry -> entry.getValue() == 1)
+                .map(Map.Entry::getKey)
+                .toArray(String[]::new);
+
+        System.out.println("Unique Fruits: " + Arrays.toString(UniqueFruits2));
+
+
+        //    Find: 2. Duplicate elements
+        List<String> duplicatesFruits = input2.stream()
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        LinkedHashMap::new,
+                        Collectors.counting()
+                ))
+                .entrySet().stream()
+                .filter(entry -> entry.getValue() >1)
+                .map(Map.Entry::getKey)
+                .toList();
+
+        System.out.println("Duplicates Fruits: " + duplicatesFruits);
+
+        //    Find: 3. First non-repeating element
+
+        String firstNonRepeating2 = input2.stream()
+                .collect(Collectors.groupingBy(
+                        n -> n,
+                        LinkedHashMap::new,
+                        Collectors.counting()
+                ))
+                .entrySet().stream()
+                .filter(entry -> entry.getValue() == 1)
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElse(null);
+
+        System.out.println("First Non-Repeating Element: " + firstNonRepeating2);
     }
 
 }
